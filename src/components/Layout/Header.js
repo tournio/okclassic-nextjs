c720e8d75c3034ee0be96ac1e38a4240eb6e5a95
Scott Stebleton <scott@stebleton.net>
2025-01-18 13:05:06 -0500

    Update header info to remove year and be more about IGBO.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-export const siteTitle = 'OK Classic 2024 - Oklahoma City, OK';
+export const siteTitle = 'OKClassic - An IGBO tournament';
 
 const Header = () => (
   <Head>
@@ -8,7 +8,7 @@ const Header = () => (
     <meta httpEquiv={"Content-Type"} content={"text/html; charset=utf-8"} />
     <meta name={"title"} content={siteTitle} />
     <meta name={"viewport"} content={"width=device-width, initial-scale=1.0, shrink-to-fit=yes"} />
-    <meta name={'description'} content={'The OK Classic is an IGBO-affiliated bowling tournament, taking place each April in Oklahoma City.'} />
+    <meta name={'description'} content={'The OKClassic is an IGBO-affiliated bowling tournament, taking place each April in Oklahoma City.'} />
     <meta name={'twitter:card'} content={'summary_large_image'} />
     <link rel="icon" type="image/x-icon" href="/images/logo-50x50.jpg" />
   </Head>
