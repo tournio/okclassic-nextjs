ccfafadaa8e24d8933527545f66b041a9d63f46c
Scott Stebleton <scott@stebleton.net>
2026-01-08 19:06:44 -0500

    remove unneeded code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,6 @@ import {Html, Head, Main, NextScript} from "next/document";
 const Document = () => (
   <Html>
     <Head>
-      <link
-        href={'https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap'}
-        rel={'stylesheet'}/>
     </Head>
     <body>
       <div className={`top-image-background`}></div>
